d13d03523fe27ffda20f98ad57302ec3bfc86ec8
wglim <wglim@ematsoft.co.kr>
2025-02-13 13:39:44 +0900

    log 추가

--- myapplication2/src/main/java/com/ematsoft/myapplication2/MainActivity.java
+++ myapplication2/src/main/java/com/ematsoft/myapplication2/MainActivity.java
@@ -1,6 +1,7 @@
 package com.ematsoft.myapplication2;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +16,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        Log.i("MainActivity", "onCreate");
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
